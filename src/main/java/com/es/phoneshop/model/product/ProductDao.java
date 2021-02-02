b8d601e3b318b08ab18fd929f425d5bb135d9255
Denis Martinevski <DenisMartinevski@gmail.com>
2021-02-02 21:43:28 +0300

    Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ProductDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface ProductDao {
     Product getProduct(Long id);
     List<Product> findProducts();
+    List<Product> getProducts();
     void save(Product product);
     void delete(Long id);
 }
